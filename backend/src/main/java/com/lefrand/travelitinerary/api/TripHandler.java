11f9a841da9f66e34429538301eaf0b68f746748
LefrandSM <lefrandseptiandm@gmail.com>
2026-06-14 18:45:24 +0200

    feat: create TripHandler for reading all trips

---
+++ backend/src/main/java/com/lefrand/travelitinerary/api/TripHandler.java
@@ -0,0 +1,38 @@
+package com.lefrand.travelitinerary.api;
+
+import com.lefrand.travelitinerary.dao.TripDao;
+import com.lefrand.travelitinerary.model.Trip;
+import com.lefrand.travelitinerary.util.JsonUtil;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.OutputStream;
+import java.util.List;
+
+public class TripHandler implements HttpHandler {
+    private final TripDao tripDao = new TripDao();
+
+    @Override
+    public void handle(HttpExchange exchange) {
+        try {
+            if(exchange.getRequestMethod().equals("GET")) {
+
+                // 1. Read from DB
+                List<Trip> trips = tripDao.readAllTrips();
+
+                // 2. Convert to JSON
+                String json = JsonUtil.toJson(trips);
+
+                // 3. Send response
+                exchange.getResponseHeaders().add("Content-Type", "application/json");
+                exchange.sendResponseHeaders(200, json.getBytes().length);
+
+                OutputStream os = exchange.getResponseBody();
+                os.write(json.getBytes());
+                os.close();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
