ebc0dfd88d8b14aafbd64845c6efe28267aeff26
LefrandSM <lefrandseptiandm@gmail.com>
2026-06-14 18:33:03 +0200

    chore: add JSON serialization utility for API responses

---
+++ backend/src/main/java/com/lefrand/travelitinerary/util/JsonUtil.java
@@ -0,0 +1,11 @@
+package com.lefrand.travelitinerary.util;
+
+import com.google.gson.Gson;
+
+public class JsonUtil {
+    private static final Gson gson = new Gson();
+
+    public static String toJson(Object obj) {
+        return gson.toJson(obj);
+    }
+}
