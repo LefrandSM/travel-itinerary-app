ce24fb5197d13b78699cebdb09181ad89fa2e49a
LefrandSM <lefrandseptiandm@gmail.com>
2026-06-14 18:52:03 +0200

    chore: set up main server and send trip data from db to postman

--- backend/src/main/java/com/lefrand/travelitinerary/App.java
+++ backend/src/main/java/com/lefrand/travelitinerary/App.java
@@ -1,17 +1,14 @@
 package com.lefrand.travelitinerary;
 
-import com.lefrand.travelitinerary.config.DatabaseConnection;
-import com.lefrand.travelitinerary.dao.TripDao;
-import com.lefrand.travelitinerary.model.Trip;
+import com.lefrand.travelitinerary.api.TripHandler;
+import com.sun.net.httpserver.HttpServer;
 
-import java.sql.Date;
-import java.sql.SQLException;
-import java.time.LocalDate;
+import java.net.InetSocketAddress;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    static void main() {
+public class App {
+    static void main() throws Exception {
 
 //        Create
 //        Trip trip = new Trip();
@@ -30,5 +27,11 @@ public class Main {
 //        for(Trip t : trips.readAllTrips()) {
 //            System.out.println(t.getName());
 //        }
+
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        server.createContext("/trips", new TripHandler());
+        server.start();
+        System.out.println("Server running on http://localhost:8080");
+
     }
 }
